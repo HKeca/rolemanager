267ed992f53a4fea771437cb0bce0d94e5551f90
Hunter Keca <hunterkeca@gmail.com>
2021-04-01 01:51:58 +0000

    fix: add discord intents to client

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES'] } });
 const ytdl = require('ytdl-core');
 require('dotenv').config();
 
